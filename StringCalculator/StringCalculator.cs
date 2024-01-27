47eed4b9f29e093fda32f779f837a830403865e3
Pedro Osternack Correa <pedrost.rj@gmail.com>
2024-01-27 17:35:54 -0500

    removing Add code

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -4,13 +4,6 @@ public class StringCalculator
 {
     public int Add(string input)
     {
-
-        var inputs = input.Split(new[] { ',', '\n' }).Select(int.Parse);
-        var negatives = inputs.Where(i => i < 0);
-        if (negatives.Any())
-        {
-            throw new Exception($"negatives not allowed: {string.Join(",", negatives)}" );
-        }
-        return 0;
+        throw new NotImplementedException();
     }
 }
